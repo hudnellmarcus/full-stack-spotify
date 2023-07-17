7e7aa9473716ce5221c9e7a2f3cefe77f56736c4
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2023-07-16 21:47:10 -0700

    basic authentication w/ supabase completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -14,6 +15,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const authModal = useAuthModal();
   const router = useRouter();
 
   const handleLogout = () => {
@@ -119,7 +121,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             {/* content will be dynamic */}
             <div>
               <Button
-              onClick={() => {}}
+              onClick={authModal.onOpen}
                 className="
                 bg-transparent
                 text-neutral-300
@@ -130,7 +132,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             </div>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="
                 bg-white
                px-6
